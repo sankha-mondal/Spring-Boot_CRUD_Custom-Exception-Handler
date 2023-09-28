edd5719d1086861f9d564f7ef552f38986413480
sankha-mondal <99641675+sankha-mondal@users.noreply.github.com>
2023-09-28 21:01:06 +0530

    Spring-Boot CRUD Custom-Exception-Handler

--- Spring-Boot_CRUD_Custom-Exception-Handler/src/main/java/com/exception_custom/ResourceNotFoundException.java
+++ Spring-Boot_CRUD_Custom-Exception-Handler/src/main/java/com/exception_custom/ResourceNotFoundException.java
@@ -3,7 +3,6 @@ package com.exception_custom;
 public class ResourceNotFoundException extends RuntimeException {
 
 	  private static final long serialVersionUID = 1L;
-
 	  public ResourceNotFoundException(String msg) {
 	    super(msg);
 	  }
